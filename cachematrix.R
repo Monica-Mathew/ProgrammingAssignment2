d18db79e63e947a5e8a1a940169cf5e7426a8a63
Monica-Mathew <monica.m.mathew@gmail.com>
2020-04-20 01:43:25 +0530

    My first commit

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,41 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = matrix()) { ##can cache the inverse
+  inver<-NULL ## holds the value of the inverse matrix
+  set<-function(y){
+    x<<-y
+    inver<<-NULL ##<<- assign a value to an object in an environment that is different from the current environment
+  }
+  get <- function(){ ## returns the matrix x
+    x
+  }
+  setinverse <- function(inverse)
+  {
+    inver <<- inverse
+  }
+  getinverse <- function() ## returns the value of the matrix argument
+  {
+    x
+  }
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  inver<-x$getinverse()
+  if(!is.null(inver))
+  {
+    return(inver)
+    message("The cached inverse of the matrix returns")
+  }
+  data<-x$get() ##get the matrix
+  inver<-solve(data, ...) ## get the inverse of the matrix
+  x$setinverse(x)  ##set the inverse
+  x
 }
